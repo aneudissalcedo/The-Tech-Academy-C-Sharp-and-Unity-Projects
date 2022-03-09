ebb0a76239e7ee341e121b8bb9fdabe9fe8ca597
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-09 18:17:09 -0500

    Added changes to the iteration.cs file, now it identifies the duplicates.

--- Basic_C#_Programs/Iterations/Iteration.cs
+++ Basic_C#_Programs/Iterations/Iteration.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class Iteration
@@ -13,7 +14,9 @@ class Iteration
 
         for (int i = 0; i < namesArray.Length; i++)
         {
-            Console.WriteLine(namesArray[i] + " also like to play " + text);
+            //Console.WriteLine(namesArray[i] + " also like to play " + text);
+            namesArray[i] = namesArray[i] + " also like to play " + text;
+            Console.WriteLine(namesArray[i]);
         }
 
         //Fixed loop
@@ -71,27 +74,66 @@ class Iteration
         }
 
         Console.WriteLine("\n--------------------------------------\n");
-        List<string> games = new List<string>();
-        games.Add("Pokemon");
-        games.Add("Digimon");
-        games.Add("Borderlands");
-        games.Add("Hades");
-        games.Add("Tetris");
 
+        //Part 5: This one is missing. A list of strings that has at least 
+        //    two identical strings in the list. Ask the user to select text 
+        //    to search for in the list. This one iterates like in part 4 
+        //    but there are two identical strings and you print out those indexes if chosen.
+
+        List<string> games = new List<string>();
+        games.Add("pokemon is a catching creature monster game.");
+        games.Add("digimon is a digital creature monster game.");
+        games.Add("borderlands has a funny story main line.");
+        games.Add("hades, one of the newest game in the market place.");
+        games.Add("tetris is a classic game.");
 
         string textGames = "";
-        Console.WriteLine("Search a game in the list containing:");
-        textGames = Console.ReadLine();
+        int selection = 0;
+        //Console.WriteLine("Search a game in the list containing:");
+        //textGames = Console.ReadLine();
+        do
+        {
+            Console.WriteLine("Select a text to search in the list:");
+            Console.WriteLine("1)creature \n2)funny \n3)monster \n4)classic \n5)market");
+            selection = Convert.ToInt32(Console.ReadLine());
+
+        } while (selection <= 0 || selection > 5);
+
+        switch(selection)
+        {
+            case 1:
+                textGames = "creature";
+                break;
+            case 2:
+                textGames = "funny";
+                break;
+            case 3:
+                textGames = "monster";
+                break;
+            case 4:
+                textGames = "classic";
+                break;
+            case 5:
+                textGames = "market";
+                break;
+        }
+
+        //var duplicateGames = games
+        //        .GroupBy(x => x)
+        //        .Where(g => g.Count() > 1)
+        //        .Select(g => g.Key);
 
         for (int i = 0; i < games.Count; i++)
         {
             if (games[i].Contains(textGames))
             {
-                Console.WriteLine("Index: {0}", i);
+                //Console.WriteLine(games[i] + " is a duplicate with index {0}", i);
+                Console.WriteLine("The word {0} is in the index: {1}", textGames, i);
             }
             else
             {
-                Console.WriteLine("There is no game match with index {0}", i);
+                //Console.WriteLine("There is no duplicate game with index {0}", i);
+                Console.WriteLine("The word {0} is not in the index: {1}", textGames, i);
             }
         }
 
@@ -109,7 +151,14 @@ class Iteration
         //}
 
         Console.WriteLine("\n--------------------------------------\n");
+
+        //Part 6: You just iterate through the list and print all the values out. 
+        //In this one you need to create a foreach loop that evaluates each item 
+        //in the list, and displays a message showing the string and whether or not 
+        //it has already appeared in the list.
+
         List<string> words = new List<string>();
+        List<string> duplicates = new List<string>();
         words.Add("pedro");
         words.Add("looking");
         words.Add("looking");
@@ -118,7 +167,15 @@ class Iteration
 
         foreach (var word in words)
         {
-            Console.WriteLine(word);
+            if (duplicates.Contains(word))
+            {
+                Console.WriteLine(word + " already appeared in the list.");
+            }
+            else
+            {
+                Console.WriteLine(word + " first time in the list.");
+                duplicates.Add(word);
+            }
         }
 
 
