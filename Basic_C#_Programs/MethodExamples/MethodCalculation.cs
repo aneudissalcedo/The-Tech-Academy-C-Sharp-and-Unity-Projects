a6901efa16f690ba374ad591a1df170150ef62c1
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-11 17:22:39 -0500

    Added a simple math operation using methods from another class file.

---
+++ Basic_C#_Programs/MethodExamples/MethodCalculation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodExamples
+{
+    public class MethodCalculation
+    {
+        public int SumInt(int a)
+        {
+            a += a;
+            return a;
+        }
+
+        public int SubInt(int a)
+        {
+            a -= a;
+            return a;
+        }
+
+        public int MultInt(int a)
+        {
+            a *= a;
+            return a;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            MethodCalculation methodCalculation = new MethodCalculation();
+            int inputNumber = 0;
+
+            Console.WriteLine("Please enter a number to follow up with addition, substraction and multiplication:");
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(inputNumber + " + " + inputNumber + " = " + methodCalculation.SumInt(inputNumber));
+            Console.WriteLine(inputNumber + " - " + inputNumber + " = " + methodCalculation.SubInt(inputNumber));
+            Console.WriteLine(inputNumber + " * " + inputNumber + " = " + methodCalculation.MultInt(inputNumber));
+
+            Console.WriteLine("\nPress Enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
