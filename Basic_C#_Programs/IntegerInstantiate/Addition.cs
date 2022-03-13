b674dc6779d769b279037a7cbb6f9f0f3b114529
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-13 12:05:26 -0400

    Added different operations with the same method name, it vary by the parameters its passing by.

---
+++ Basic_C#_Programs/IntegerInstantiate/Addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegerInstantiate
+{
+    public class Addition
+    {
+        public int AdditionIntegers(int a)
+        {
+            a += a;
+            return a;
+        }
+
+        public int AdditionIntegers(decimal a)
+        {
+            a -= a;
+            return Convert.ToInt32(a);
+        }
+
+        public int AdditionIntegers(string s)
+        {
+            int a;
+            a = Convert.ToInt32(s);
+            a *= a;
+
+            return a;
+        }
+    }
+}
---
+++ Basic_C#_Programs/IntegerInstantiate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntegerInstantiate
+{
+    class Program
+    {
+        static void Main()
+        {
+            Addition addition = new Addition();
+            int a = 5;
+            decimal b = 5m;
+            string s = "7";
+
+            Console.WriteLine("Passing in an integer: " + addition.AdditionIntegers(a));
+            Console.WriteLine("Passing in a decimal: " + addition.AdditionIntegers(b));
+            Console.WriteLine("Passing in a decimal: " + addition.AdditionIntegers(s));
+
+            Console.WriteLine("\nPress Enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
