70bc0d34fdd92b8434f3ac6c9f329ad963e85c70
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-14 13:52:27 -0400

    Fixed the TwoParametersMethod, now it can calculate two integers without adding the second parameter.

--- Basic_C#_Programs/VoidMethod/Program.cs
+++ Basic_C#_Programs/VoidMethod/Program.cs
@@ -11,6 +11,10 @@ namespace VoidMethod
             int firstInteger = 3;
             int secondInteger = 7;
 
+            //Passing in without parameters name (parameter order required)
+            voidMethod.VoidMethodIntegers(firstInteger, secondInteger);
+
+            //Passing in parameters name (parameter order is not required)
             voidMethod.VoidMethodIntegers(a: firstInteger, b: secondInteger);
 
             Console.WriteLine("\nPress Enter Key to continue...");
