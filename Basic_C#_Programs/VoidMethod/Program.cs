3e27b2510f9d50110e9f0a4825558fa650c9a070
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-14 11:49:54 -0400

    Added a void method to the repository from another class file.

---
+++ Basic_C#_Programs/VoidMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            VoidMethod voidMethod = new VoidMethod();
+            int firstInteger = 3;
+            int secondInteger = 7;
+
+            voidMethod.VoidMethodIntegers(firstInteger, secondInteger);
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidMethod/VoidMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod
+{
+    public class VoidMethod
+    {
+        public void VoidMethodIntegers(int a = 0, int b = 0)
+        {
+            b = b + a + 5;
+            Console.WriteLine(b);
+        }
+    }
+}
