84fcd343296a0d4ca30cd34f76e8ac295414af72
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-15 18:09:04 -0400

    Created a superclass method and executed an example of it to see the utilization of inheritance.

---
+++ Basic_C#_Programs/SuperClassMethod/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperClassMethod
+{
+    public class Employee : Person
+    {
+        public int EmployeeID { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/SuperClassMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SuperClassMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            
+            employee.SayName();
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
