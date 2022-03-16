8dd404c3236b19e360b71c44288c1552dfd816f4
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-16 14:37:59 -0400

    Added a polymorphism implementation with interface to the repository.

---
+++ Basic_C#_Programs/Polymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main()
+        {
+            IQuittable quittable = new Employee();
+
+            quittable.Quit();
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
