8b69204c51db7a3e4cdd4ffcf60e59cb7a30ac0c
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-16 14:00:19 -0400

    Added example of abstract class implementation to the repository.

---
+++ Basic_C#_Programs/AbstractClass/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        public override void SayMethod()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Basic_C#_Programs/AbstractClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            employee.SayMethod();
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
