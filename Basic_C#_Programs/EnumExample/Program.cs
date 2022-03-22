ad76ec71f24df6a04d55b451e6b8d6fed51d86fe
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-22 16:49:48 -0400

    Added example of Enum to the repository.

---
+++ Basic_C#_Programs/EnumExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnumExample
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            monday, tuesday, wednesday, thursday, friday, saturday, sunday
+        }
+
+        static void Main()
+        {
+            string userInput = "";
+            WeekDays weekDays;
+
+            Console.WriteLine("Please enter the current day of the week:");
+            userInput = Console.ReadLine().ToLower();
+
+            try
+            {
+                weekDays = (WeekDays)Enum.Parse(typeof(WeekDays), userInput);
+                Console.WriteLine("Today is {0}", weekDays);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\nPlease enter an actual day of the week.");
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.WriteLine("\nPress Enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
