e7d8ced8c6a38a23203bb6a61726eb90685913f8
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-22 11:33:16 -0400

    Added a Generic class example to the repository.

---
+++ Basic_C#_Programs/GenericExample/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericExample
+{
+    public class Employee<T>
+    {
+        public List<T> things;
+
+        public Employee()
+        {
+            things = new List<T>();
+        }
+
+        public void AddProperty(T newProperty)
+        {
+            things.Add(newProperty);
+        }
+
+        public void PrintAllThings()
+        {
+            foreach (var thing in things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/GenericExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.AddProperty("First");
+            employeeString.AddProperty("Second");
+            employeeString.AddProperty("Third");
+
+            Employee<int> employeeInt = new Employee<int>();
+            employeeInt.AddProperty(1);
+            employeeInt.AddProperty(2);
+            employeeInt.AddProperty(3);
+
+            employeeString.PrintAllThings();
+            employeeInt.PrintAllThings();
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
