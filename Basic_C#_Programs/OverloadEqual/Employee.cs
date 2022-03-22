833104f147c41067cb097ef2cd27d322a091dedf
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-22 10:16:53 -0400

    Added overload equal operator

---
+++ Basic_C#_Programs/OverloadEqual/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadEqual
+{
+    public class Employee : Person
+    {
+        public int EmployeeID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.EmployeeID == employee2.EmployeeID;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.EmployeeID != employee2.EmployeeID;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/OverloadEqual/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OverloadEqual
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            employee1.EmployeeID = 1;
+            employee2.EmployeeID = 1;
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Employee 1 is equal to Employee 2", employee1, employee2);
+            }
+            else 
+            {
+                Console.WriteLine("Employee 1 is not equal to Employee 2", employee1, employee2);
+            }
+
+            Console.WriteLine("\nPress Enter Key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
