e9254cece6afa7ca949bb53cd98c945e54fbc79b
aneudissalcedo <aneudissalcedo@gmail.com>
2022-03-26 17:04:51 -0400

    Added a DataTime example to the repository.

---
+++ Basic_C#_Programs/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DateTimeExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("\nPlease enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, the time will be: {1}", number, DateTime.Now.AddHours(number));
+
+            Console.WriteLine("\nPress Enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/Dealer.cs
+++ Basic_C#_Programs/TwentyOne/Dealer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -14,7 +15,13 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Aneudis Salcedo\Documents\Classes\The Tech Academy\Assignments\Git\C# and Unity Projects\Basic_C#_Programs\TwentyOne\Logs\Log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
